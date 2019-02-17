55095ab2be0f3e6c58ee10718060d006723bb424
sooonk <sonia@przenioslo.eu>
2019-02-17 10:31:35 +0100

    Added header and footer in react

---
+++ src/footer.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return <div><footer>Author: Sonia Przeniosło</footer></div>
+    }
+}
+
+export default Footer;
---
+++ src/header.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Header extends React.Component {
+    render() {
+        return <div><h1>React App - Issue 5</h1></div>
+    }
+}
+
+export default Header;
